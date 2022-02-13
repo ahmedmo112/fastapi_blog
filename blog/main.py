f3af23c475b5a422308bae529e7fa98ec704d7ec
ahmed <amhk11827@gmail.com>
2022-02-13 02:42:09 +0200

    update

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,7 @@
 
 from statistics import mode
 from turtle import title
+from typing import List
 from fastapi import Depends, FastAPI ,status , Response ,HTTPException
 from . import schemas , models
 from .database import SessionLocal, engine
@@ -30,13 +31,13 @@ def create(request: schemas.Blog, db :Session = Depends(get_db)):
     return new_blog
     
 
-@app.get('/blog')
-def all(db :Session = Depends(get_db)):
+@app.get('/blog',response_model=List[schemas.ShowBlog]) # response_model=List[schemas.ShowBlog] it will get just the title of blogs
+def all(db :Session = Depends(get_db)):                 # as we do that at schemas.py so we can costomize it
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=200)
+@app.get('/blog/{id}', status_code=200,response_model=schemas.ShowBlog)
 def show(id,response: Response,db :Session = Depends(get_db), ):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -45,3 +46,33 @@ def show(id,response: Response,db :Session = Depends(get_db), ):
         # return {'detail': f"Blog with the id {id} is not avaialable"}
     
     return blog
+
+
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id,response: Response,db :Session = Depends(get_db),):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Blog with id {id} not found')
+
+    blog.delete(synchronize_session=False)
+
+    db.commit()
+
+    return {'detail': f'blog with id {id} has been deleted'}
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)    
+def update(id,request: schemas.Blog ,db :Session = Depends(get_db)):
+    update_blog = models.Blog(
+        title = request.title,
+        body = request.body
+    )
+    blog= db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Blog with id {id} not found')
+
+    blog.update({'title':request.title,'body':request.body},synchronize_session=False)
+
+    db.commit() 
+
+    return {'detail':"updated"}
--- blog/schemas.py
+++ blog/schemas.py
@@ -3,3 +3,9 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title:str
     body:str
+
+class ShowBlog(BaseModel): # we use it in response_model to view the data as what we want
+    title:str
+    body:str
+    class Config():
+        orm_mode = True
